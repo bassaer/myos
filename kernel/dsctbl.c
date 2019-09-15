130a2ce27ae17fb6734c52e25ff011e160c6dd40
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-09-16 03:03:45 +0900

    add comment

--- kernel/dsctbl.c
+++ kernel/dsctbl.c
@@ -13,8 +13,7 @@ void init_gdtidt() {
   // メモリ全体
   set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, AR_DATA32_RW);
   // カーネル
-  //set_segmdesc(gdt + 2, KRN_LIMIT, KRN_ADDR, AR_CODE32_ER);
-  set_segmdesc(gdt + 2, 0xffffffff, 0x00000000, AR_CODE32_ER);
+  set_segmdesc(gdt + 2, KERN_LIMIT, KERN_ADDR, AR_CODE32_ER);
   load_gdtr(GDT_LIMIT, GDT_ADDR);
 
   for (i = 0; i <= IDT_LIMIT / 8; i++) {
