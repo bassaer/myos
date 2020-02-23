4fe0cf2fa9394ace44543be49f0387d2322988a3
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-02-23 23:01:27 +0900

    add pgtable functions

--- include/pgtable.h
+++ include/pgtable.h
@@ -1,8 +1,23 @@
 #ifndef MYOS_PGTABLE_H
 #define MYOS_PGTABLE_H
 
-
 typedef unsigned long page_table_entry;
 typedef unsigned long page_directory_entry;
+
+void set_pte_flag(page_table_entry *pte, unsigned int flags);
+
+void clear_pte_flag(page_table_entry *pte, unsigned int flags);
+
+void set_pte_paddr(page_table_entry *pte, unsigned long paddr);
+
+void* get_pte_paddr(page_table_entry *pte);
+
+void set_pde_flag(page_directory_entry *pde, unsigned int flags);
+
+void clear_pde_flag(page_directory_entry *pde, unsigned int flags);
+
+void set_pde_paddr(page_directory_entry *pde, unsigned long paddr);
+
+void* get_pde_paddr(page_directory_entry *pde);
 
 #endif
--- mm/pgtable.c
+++ mm/pgtable.c
@@ -1,2 +1,39 @@
 #include <pgtable.h>
 
+void set_pte_flag(page_table_entry *pte, unsigned int flags) {
+  *pte |= flags;
+}
+
+void clear_pte_flag(page_table_entry *pte, unsigned int flags) {
+  *pte &= ~flags;
+}
+
+void set_pte_paddr(page_table_entry *pte, unsigned long paddr) {
+  // 下位12bitをマスク
+  paddr &= 0xfffff000;
+  *pte |= paddr;
+}
+
+void* get_pte_paddr(page_table_entry *pte) {
+  // 下位12bitをマスク
+  return (void *)(*pte & 0xfffff000);
+}
+
+void set_pde_flag(page_directory_entry *pde, unsigned int flags) {
+  *pde |= flags;
+}
+
+void clear_pde_flag(page_directory_entry *pde, unsigned int flags) {
+  *pde &= ~flags;
+}
+
+void set_pde_paddr(page_directory_entry *pde, unsigned long paddr) {
+  // 下位12bitをマスク
+  paddr &= 0xfffff000;
+  *pde |= paddr;
+}
+
+void* get_pde_paddr(page_directory_entry *pde) {
+  // 下位12bitをマスク
+  return (void *)(*pde & 0xfffff000);
+}
