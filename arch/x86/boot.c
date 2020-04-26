750dfa73b2b37636688b97b1242a359e7835e53b
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-04-26 19:02:17 +0900

    fix ExitBootServices

--- arch/x86/boot.c
+++ arch/x86/boot.c
@@ -9,10 +9,7 @@ void halt() {
   while (1) __asm__ volatile("hlt");
 }
 
-void ls(EFI_SYSTEM_TABLE *SystemTable) {
-  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
-  EFI_GUID guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
-  SystemTable->BootServices->LocateProtocol(&guid, NULL, (void **)&fs);
+void ls(EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs) {
   EFI_FILE_PROTOCOL *root;
   EFI_STATUS status = fs->OpenVolume(fs, &root);
   if (status != EFI_SUCCESS) {
@@ -25,7 +22,6 @@ void ls(EFI_SYSTEM_TABLE *SystemTable) {
     INTN buf_size = BUF_SIZE;
     status = root->Read(root, &buf_size, file);
     if (status != EFI_SUCCESS) {
-      SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Read error!\r\n");
       break;
     }
     if (!buf_size) {
@@ -33,57 +29,48 @@ void ls(EFI_SYSTEM_TABLE *SystemTable) {
     }
     printf(L"index: %d, bufsize: %d\r\n", index, buf_size);
     printf(L"name: %s\r\n", file->FileName);
-    //printf(L"size: %d\r\n", file->Size);
-    //printf(L"physicalSize: %d\r\n", file->PhysicalSize);
     index++;
-  }
-  if (index == 0) {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Not Found\r\n");
-  } else {
-    printf(L"found %e files", index);
   }
   root->Close(root);
 }
 
-//EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-EFI_STATUS efi_main(void *ImageHandle __attribute__ ((unused)), EFI_SYSTEM_TABLE *SystemTable) {
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
   init_event(SystemTable->BootServices);
   init_console(SystemTable->ConIn, SystemTable->ConOut);
   printf( L"loading kernel...\r\n");
-  UINTN MemoryMapSize = 4800;
-  EFI_MEMORY_DESCRIPTOR MemoryMap[4800];
+
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+  EFI_GUID guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+  SystemTable->BootServices->LocateProtocol(&guid, NULL, (void **)&fs);
+  ls(fs);
+
+  UINTN MemoryMapSize = 0;
+  EFI_MEMORY_DESCRIPTOR *MemoryMap = NULL;
   UINTN MapKey;
   UINTN DescriptorSize;
   UINT32 DescriptorVersion;
 
-  EFI_STATUS status = EFI_SUCCESS;
-  do {
-    status = SystemTable->BootServices->GetMemoryMap(&MemoryMapSize, (EFI_MEMORY_DESCRIPTOR *)MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion);
-    if (status == EFI_BUFFER_TOO_SMALL) {
-      EFI_STATUS allocStatus = SystemTable->BootServices->AllocatePool(EfiLoaderData, MemoryMapSize, (void **)&MemoryMap);
-      if (allocStatus != EFI_SUCCESS) {
-      }
-    } else if (status != EFI_SUCCESS) {
-    }
-  } while(status != EFI_SUCCESS);
-
+  EFI_STATUS status = SystemTable->BootServices->GetMemoryMap(&MemoryMapSize, (EFI_MEMORY_DESCRIPTOR *)MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion);
+  if (status != EFI_BUFFER_TOO_SMALL) {
+    return status;
+  }
+  MemoryMapSize += 2 * DescriptorSize;
+  status = SystemTable->BootServices->AllocatePool(EfiLoaderData, MemoryMapSize, (void **)&MemoryMap);
   if (status != EFI_SUCCESS) {
-    halt();
+    return status;
+  }
+  status = SystemTable->BootServices->GetMemoryMap(&MemoryMapSize, (EFI_MEMORY_DESCRIPTOR *)MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion);
+  if (status != EFI_SUCCESS) {
+    return status;
   }
 
-  //printf(L"[info] MapKey: %d, MemoryMapSize: %d, vadd: %d, padd: %d\r\n", MapKey, MemoryMapSize, MemoryMap.VirtualStart, MemoryMap.PhysicalStart);
-
+  status = SystemTable->BootServices->ExitBootServices(ImageHandle, MapKey);
+  if (status != EFI_SUCCESS) {
+    return status;
+  }
   //__asm__ volatile("jmp *%0"::"r"((UINTN)0x0000000000001000));
 
-  //status = SystemTable->BootServices->ExitBootServices(ImageHandle, MapKey);
-
-  ls(SystemTable);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"jump to kernel...\r\n");
   halt();
-
-  //printf(L"Start Shell\r\n");
-
-  //start_shell();
 
   return EFI_SUCCESS;
 }
--- include/err.h
+++ include/err.h
@@ -1,16 +1,21 @@
 #ifndef MYOS_ERR_H
 #define MYOS_ERR_H
 
-#define EFI_SUCCESS 0
-#define EFI_LOAD_ERROR 1
-#define EFI_INVALID_PARAMETER 2
-#define EFI_UNSUPPORTED 3
-#define EFI_BAD_BUFFER_SIZE 4
-#define EFI_BUFFER_TOO_SMALL 5
-#define EFI_NOT_READY 6
-#define EFI_DEVICE_ERROR 7
-#define EFI_WRITE_PROTECTED 8
-#define EFI_OUT_OF_RESOURCES 9
-#define EFI_VOLUME_CORRUPTED 10
+#include <type.h>
+
+#define MAX_BIT                     0x8000000000000000ULL
+#define ENCODE_ERROR(StatusCode)    ((EFI_STATUS)(MAX_BIT | (StatusCode)))
+
+#define EFI_SUCCESS                 0
+#define EFI_LOAD_ERROR              ENCODE_ERROR(1)
+#define EFI_INVALID_PARAMETER       ENCODE_ERROR(2)
+#define EFI_UNSUPPORTED             ENCODE_ERROR(3)
+#define EFI_BAD_BUFFER_SIZE         ENCODE_ERROR(4)
+#define EFI_BUFFER_TOO_SMALL        ENCODE_ERROR(5)
+#define EFI_NOT_READY               ENCODE_ERROR(6)
+#define EFI_DEVICE_ERROR            ENCODE_ERROR(7)
+#define EFI_WRITE_PROTECTED         ENCODE_ERROR(8)
+#define EFI_OUT_OF_RESOURCES        ENCODE_ERROR(9)
+#define EFI_VOLUME_CORRUPTED        ENCODE_ERROR(10)
 
 #endif
